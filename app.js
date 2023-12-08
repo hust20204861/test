52991daf37f8977fe49a27b0a3045fa4328a670f
Nguyen Viet Van <nguyenvietvan2002@gmail.com>
2023-12-08 23:24:55 +0700

    vievaaa

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const cloudinary = require('cloudinary')
+
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const fileUpload = require('express-fileupload')
+const dotenv = require('dotenv');
+const path = require('path')
+
+const errorMiddleware = require('./middlewares/errors')
+
+// Setting up config file 
+if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
+// dotenv.config({ path: 'backend/config/config.env' })
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser())
+app.use(fileUpload());
+
+//setting up cloudinary
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
+})
+
+
+// Import all routes
+const products = require('./routes/product');
+const auth = require('./routes/auth');
+const payment = require('./routes/payment');
+const order = require('./routes/order');
+
+
+app.use('/api/v1', products)
+app.use('/api/v1', auth)
+app.use('/api/v1', payment)
+app.use('/api/v1', order)
+
+if (process.env.NODE_ENV === 'PRODUCTION') {
+    app.use(express.static(path.join(__dirname, '../front/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../front/build/index.html'))
+    })
+}
+
+
+// Middleware to handle errors
+app.use(errorMiddleware);
+
+module.exports = app
